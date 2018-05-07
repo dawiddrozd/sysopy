0185b348bdc818a167af784f057cb30d3280c09b
Dawid Drozd <ddrozdu@gmail.com>
2018-05-07 13:22:10 +0200

    still not working

--- cw07/zad1/barber.c
+++ cw07/zad1/barber.c
@@ -5,8 +5,20 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <sys/sem.h>
+#include <fcntl.h>
+#include <signal.h>
 #include "../common/common_utils.h"
 #include "barber_utils.h"
+
+int sem_id;
+int shm_id;
+
+void stop_handler(int signum) {
+    printf(MAG "[Barber] Shop closed. Goodbye!\n" RESET);
+    shmctl(shm_id, IPC_RMID, NULL);
+    semctl(sem_id, 0, IPC_RMID);
+    exit(0);
+}
 
 pid_t queue_pop(Barbershop *barbershop) {
     if (barbershop->clients_waiting > 0) {
@@ -72,4 +84,41 @@ void run(int nr_seats, Barbershop *barbershop, int sem_id) {
         }
         dec_sem(sem_id);
     }
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "One argument expected\n");
+        exit(1);
+    }
+
+    int nr_seats = to_int(argv[1]);
+    check_exit(nr_seats < 0, "Only numbers expected as first argument");
+
+    // init shared memory
+    key_t shm_key;
+    shm_key = ftok(get_homedir(), SHM_CHAR);
+    check_exit(shm_key < 0, "Ftok failed.");
+    shm_id = shmget(shm_key, sizeof(Barbershop), IPC_CREAT | 0666);
+    check_exit(shm_id < 0, "Shmget failed.");
+    Barbershop *barber_shop;
+    barber_shop = shmat(shm_id, NULL, 0);
+    check_exit(barber_shop == (void *) -1, "Shmat failed");
+
+
+    // init semaphore
+    key_t sem_key;
+    sem_key = ftok(get_homedir(), SEM_CHAR);
+    check_exit(sem_key < 0, "Ftok failed.");
+    sem_id = semget(sem_key, 1, IPC_CREAT | S_IRWXU | S_IRWXG | S_IRWXO);
+    check_exit(sem_id < 0, "Semget failed.");
+    semctl(sem_id, 0, SETVAL, 1); //don't know if works!!
+    //inc_sem(sem_id);
+
+    barber_init(nr_seats, barber_shop);
+    signal(SIGINT, stop_handler);
+
+    run(nr_seats, barber_shop, sem_id);
+
+    return 0;
 }
--- cw07/zad1/client/client_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYSOPY_CLIENT_UTILS_H
-#define SYSOPY_CLIENT_UTILS_H
-
-#include "../common/common_utils.h"
-
-void run(int nr_clients, int nr_trimming, Barbershop *barbershop, int sem_id);
-
-#endif //SYSOPY_CLIENT_UTILS_H
