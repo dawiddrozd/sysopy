631eea4547cbbbe164572cf5c77a949c7bb6c25e
Dawid Drozd <ddrozdu@gmail.com>
2018-05-18 11:24:25 +0200

    threads support

--- cw08/zad1/main.c
+++ cw08/zad1/main.c
@@ -8,20 +8,56 @@
 #include <unistd.h>
 #include <assert.h>
 #include <math.h>
+#include <sys/time.h>
 
 #define BUFFER_SIZE 128
 #define INT_PARSE 0
 #define DOUBLE_PARSE 1
+#define NUM_PER_LINE 17
+#define MILLION 1000000
+
+#define MEASURE(print_name, test_block) { \
+    print_name; \
+    struct timeval real_start, real_end; \
+    struct rusage usage; \
+    gettimeofday(&real_start, 0); \
+    getrusage(RUSAGE_SELF, &usage); \
+    test_block; \
+    gettimeofday(&real_end, 0); \
+    getrusage(RUSAGE_SELF, &usage); \
+    printf("Real execution time: %ldµs\n", \
+    (real_end.tv_sec - real_start.tv_sec) * MILLION + real_end.tv_usec - real_start.tv_usec); \
+}
 
 typedef struct Array {
     int x_size;
     int y_size;
     int filter_size;
-    int **array;
+    int max_grey_value;
+    int **pgm;
     double **filter;
+    int **out;
 } Array;
 
-void free_all(Array array);
+typedef struct Data {
+    Array *array;
+    int first_x;
+    int last_x;
+} Data;
+
+void free_all(Array array) {
+    for (int i = 0; i < array.x_size; i++) {
+        free(array.pgm[i]);
+        free(array.out[i]);
+    }
+    free(array.pgm);
+    free(array.out);
+
+    for (int i = 0; i < array.filter_size; i++) {
+        free(array.filter[i]);
+    }
+    free(array.filter);
+}
 
 void check_exit(bool correct, const char *message) {
     if (correct) {
@@ -48,7 +84,7 @@ int to_int(char *string) {
 void print(Array array) {
     for (int i = 0; i < array.x_size; i++) {
         for (int j = 0; j < array.y_size; j++) {
-            printf("%d ", array.array[i][j]);
+            printf("%d ", array.pgm[i][j]);
         }
         printf("\n");
     }
@@ -87,13 +123,13 @@ void parse(Array *arr, FILE *stream, int type) {
         num = strtok(line, " \t");
         do {
             if (type == INT_PARSE) {
-                arr->array[x_actual][y_actual++] = to_int(num);
+                arr->pgm[x_actual][y_actual++] = to_int(num);
             } else {
                 arr->filter[x_actual][y_actual++] = strtod(num, NULL);
             }
             nums++;
             if ((y_actual >= arr->y_size && type == INT_PARSE) ||
-                    (y_actual >= arr->filter_size && type == DOUBLE_PARSE)) {
+                (y_actual >= arr->filter_size && type == DOUBLE_PARSE)) {
                 y_actual = 0;
                 x_actual++;
             }
@@ -108,12 +144,11 @@ void parse(Array *arr, FILE *stream, int type) {
     check_exit(checked, "Not proper number of numbers");
 }
 
-Array parse_pgm_file(const char *in) {
+void parse_pgm_file(Array *pgm, const char *in) {
     FILE *stream = NULL;
     char *line = NULL;
     char *num;
     ssize_t nread;
-    Array pgm;
 
     stream = fopen(in, "rw");
     check_exit(stream == NULL, "Can't open in file");
@@ -131,18 +166,20 @@ Array parse_pgm_file(const char *in) {
     num = strtok(line, " \n\t");
     int max_grey_value = to_int(num);
 
-    pgm.x_size = x_size;
-    pgm.y_size = y_size;
-    pgm.array = (int **) malloc(x_size * sizeof(int *));
+    pgm->max_grey_value = max_grey_value;
+    pgm->x_size = x_size;
+    pgm->y_size = y_size;
+    pgm->pgm = (int **) malloc(x_size * sizeof(int *));
+    pgm->out = (int **) malloc(x_size * sizeof(int *));
     for (int i = 0; i < x_size; i++) {
-        pgm.array[i] = (int *) malloc(y_size * sizeof(int));
+        pgm->pgm[i] = (int *) malloc(y_size * sizeof(int));
+        pgm->out[i] = (int *) malloc(y_size * sizeof(int));
     }
 
-    parse(&pgm, stream, INT_PARSE);
+    parse(pgm, stream, INT_PARSE);
 
     free(line);
     fclose(stream);
-    return pgm;
 }
 
 void parse_filter(const char *in, Array *filter) {
@@ -155,8 +192,10 @@ void parse_filter(const char *in, Array *filter) {
     check_exit(stream == NULL, "Can't open in file");
 
     nread = get_next_line(stream, &line);
+    check_exit(nread == -1, "Incorrect filter format.");
     num = strtok(line, " \n\t");
     int size = to_int(num);
+    check_exit(nread == -1, "Incorrect filter format.");
     filter->filter_size = size;
     filter->filter = (double **) malloc(size * sizeof(double *));
     for (int i = 0; i < size; i++) {
@@ -165,31 +204,99 @@ void parse_filter(const char *in, Array *filter) {
     parse(filter, stream, DOUBLE_PARSE);
 }
 
+int max(int one, int two) {
+    return one > two ? one : two;
+}
+
+int generate_sxy(Array *arr, int x, int y) {
+    double new_val = 0;
+    int y_idx;
+    int x_idx;
+    int c = arr->filter_size;
+    for (int i = 0; i < c; i++)
+        for (int j = 0; j < c; j++) {
+            y_idx = max(1, (x + 1) - (int) ceil((double) c / 2.0) + (i + 1)) - 1;
+            x_idx = max(1, (y + 1) - (int) ceil((double) c / 2.0) + (j + 1)) - 1;
+            new_val += arr->pgm[x_idx][y_idx] * arr->filter[i][j];
+        }
+    new_val = round(new_val);
+    return (int) new_val % arr->max_grey_value;
+}
+
+void generate(Array *arr) {
+    for (int i = 0; i < arr->x_size; i++) {
+        for (int j = 0; j < arr->y_size; j++) {
+            arr->out[i][j] = 0; /*generate_sxy(arr, i, j)*/;
+        }
+    }
+}
+
+void *run(void *param) {
+    Data *data = (Data *) param;
+    for (int i = data->first_x; i < data->last_x; i++) {
+        for (int j = 0; j < data->array->y_size; j++) {
+            data->array->out[i][j] = generate_sxy(data->array, i, j);
+        }
+    }
+    return 0;
+}
+
+void save_as(const char *out, Array *arr) {
+    char buffer[BUFFER_SIZE];
+    FILE *stream = fopen(out, "wb");
+    check_exit(stream == NULL, "Can't create file");
+    sprintf(buffer, "P2\n%d %d\n256\n", arr->x_size, arr->y_size);
+    fwrite(buffer, sizeof(char), strlen(buffer) * sizeof(char), stream);
+
+    int counter = 0;
+    for (int i = 0; i < arr->x_size; i++) {
+        for (int j = 0; j < arr->y_size; j++) {
+            sprintf(buffer, "%d ", arr->out[i][j]);
+            fwrite(buffer, sizeof(char), strlen(buffer), stream);
+            if (++counter > NUM_PER_LINE) {
+                fwrite("\n", sizeof(char), 1, stream);
+                counter = 0;
+            }
+        }
+    }
+
+    fclose(stream);
+}
+
+void threads_start(Array pgm, int nr_threads) {
+    pthread_t *threads = malloc(nr_threads * sizeof(pthread_t));
+    Data *datas = malloc(nr_threads * sizeof(Data));
+    int one_fragment;
+    for (int i = 0; i < nr_threads; i++) {
+        datas[i].array = &pgm;
+        one_fragment = datas[i].array->x_size / nr_threads;
+        datas[i].first_x = one_fragment * i;
+        datas[i].last_x = one_fragment * i + one_fragment;
+        pthread_create(&threads[i], NULL, run, &datas[i]);
+    }
+    void *returned;
+    for (int i = 0; i < nr_threads; i++) {
+        pthread_join(threads[i], &returned);
+    }
+    free(threads);
+    free(datas);
+}
+
 int main(int argc, char **argv) {
-    check_exit(argc < 4, "Incorrect number of arguments. Example: main.out 4 in.pgm filter.txt out.pgm");
+    check_exit(argc < 4, "Incorrect number of arguments. "
+            "Example: main.out 4 in.pgm filter.txt out.pgm");
 
     int nr_threads = to_int(argv[1]);
     char *in = argv[2];
     char *fltr = argv[3];
     char *out = argv[4];
     Array pgm;
-
-    pgm = parse_pgm_file(in);
+    parse_pgm_file(&pgm, in);
     parse_filter(fltr, &pgm);
-    //print(pgm);
 
+    MEASURE(printf("Execution using %d numbers of threads\n", nr_threads), threads_start(pgm, nr_threads));
+
+    save_as(out, &pgm);
     free_all(pgm);
     return 0;
-}
-
-void free_all(Array array) {
-    for(int i =0; i<array.x_size; i++) {
-        free(array.array[i]);
-    }
-    free(array.array);
-
-    for(int i=0; i<array.filter_size; i++) {
-        free(array.filter[i]);
-    }
-    free(array.filter);
 }
